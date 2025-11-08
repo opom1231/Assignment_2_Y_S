7c82e4514ecc8a3ac304f1077e5780d359617b9c
StevenA <stevenarvanitis@gmail.com>
2025-11-07 19:05:18 -0500

    Added Exec Implementation

--- interrupts.cpp
+++ interrupts.cpp
@@ -115,6 +115,35 @@ std::tuple<std::string, std::string, int> simulate_trace(std::vector<std::string
             ///////////////////////////////////////////////////////////////////////////////////////////
             //Add your EXEC output here
 
+            unsigned int program_size = get_size(program_name, external_files);
+
+            execution += std::to_string(current_time) + ", " + std::to_string(duration_intr) +", Program is " + std::to_string(program_size) + " Mb large\n";
+            current_time += duration_intr;
+
+            int load_time = 15 * program_size;
+            execution += std::to_string(current_time) + ", " + std::to_string(load_time) +", loading program into memory\n";
+
+            current_time += load_time;
+
+            if(!allocate_memory(&current)) {
+                std::cerr << "Memory allocation failed for: " << program_name << std::endl;
+            }
+            execution += std::to_string(current_time) + ", 3, marking partition as occupied\n";
+            current_time += 3;
+
+            execution += std::to_string(current_time) + ", 6, updating PCB\n";
+            current.program_name = program_name;
+            current.size = program_size;
+            current_time += 6;
+
+            execution += std::to_string(current_time) + ", 0, scheduler called\n";
+            execution += std::to_string(current_time) + ", 1, IRET\n";
+            current_time += 1;
+
+            system_status += "time: " + std::to_string(current_time) + "; current trace: " + trace_file[i] + "\n";
+            system_status += print_PCB(current, wait_queue);
+
+
 
 
             ///////////////////////////////////////////////////////////////////////////////////////////
